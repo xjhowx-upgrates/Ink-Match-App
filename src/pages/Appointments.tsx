df5bd695eb79f14b298ef120a8f660bc9d6653b0
xJHOWx <78661334+xjhowx-upgrates@users.noreply.github.com>
2025-04-16 04:10:01 -0300

    Update Appointments.tsx

--- src/pages/Appointments.tsx
+++ src/pages/Appointments.tsx
@@ -1,1 +1,13 @@
+// src/pages/Appointments.tsx
+import React from 'react';
 
+const Appointments = () => {
+  return (
+    <div>
+      <h1>Appointments Page</h1>
+      <p>Here you can manage your appointments.</p>
+    </div>
+  );
+};
+
+export default Appointments;
