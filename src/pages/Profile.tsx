b8da82b71944c8c6a1b4a707da1c51b48c14fb42
xJHOWx <78661334+xjhowx-upgrates@users.noreply.github.com>
2025-04-16 04:00:36 -0300

    Update Profile.tsx

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,1 +1,13 @@
+// src/pages/Profile.tsx
+import React from 'react';
 
+const Profile = () => {
+  return (
+    <div>
+      <h1>Your Profile</h1>
+      <p>Welcome to your profile page!</p>
+    </div>
+  );
+};
+
+export default Profile;
